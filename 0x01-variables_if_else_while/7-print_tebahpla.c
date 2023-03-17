2923b3ebf3be0db2429527a7188edf458737f813
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-17 11:27:49 -0700

    add file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet in new line.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch <= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
