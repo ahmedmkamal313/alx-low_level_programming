ead8fed3e6762e04530837bfee82441b23aeeba5
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-17 11:54:56 -0700

    file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers from 0 to 9.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
