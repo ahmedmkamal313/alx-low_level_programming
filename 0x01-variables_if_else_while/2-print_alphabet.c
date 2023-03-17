3241916558c692c2619acff6201ce726223fc654
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-17 06:36:41 -0700

    add file and description

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet in new line.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
