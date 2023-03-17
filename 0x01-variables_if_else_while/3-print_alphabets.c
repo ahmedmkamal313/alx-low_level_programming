d73cd6b47c659d756b2d5cd9873b39a1660805f4
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-17 10:10:00 -0700

    add file and describtion

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet in new line.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
