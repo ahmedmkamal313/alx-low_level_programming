70c921ddb6c960a5d19bc63c7695e0fb205575b9
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-21 03:40:54 -0700

    add file and description

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ *  print_alphabet - print the alphabet, in lowercase.
+ *  Return: 0
+ */
+
+int print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+		putchar(alph);
+		putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 0x02-functions_nested_loops readme file:
 
 0-putchar.c:  - prints _putchar, followed by a new line.
+
+1-alphabet.c: function that prints the alphabet, in lowercase, followed by a new line.
