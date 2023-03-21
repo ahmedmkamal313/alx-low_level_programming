d432ea36b07e9cedc36924a209e8d58bad95ae99
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-21 14:39:10 -0700

    add file and desc

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,8 +9,16 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hr =0 ; hr <= 23 ; hr++)
+	for (hr = 0 ; hr <= 23 ; hr++)
 	{
-		for(min = 0 ; min <= 59 ; min++)
+		for (min = 0 ; min <= 59 ; min++)
 		{
-			_putchar((
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
