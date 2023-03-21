1280692371fdb7fd50882253c8a62bfc2173f8bb
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-21 03:02:46 -0700

    start, add files and description

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar followed by a new line.
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02-functions_nested_loops readme file:
+
+0-putchar.c:  - prints _putchar, followed by a new line.
