2fd399d77977f44e7e70d4bb1f943502183f2ed6
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-21 05:47:58 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
- * Return: 0
+ *
  */
 
 void print_alphabet(void)
@@ -13,5 +13,4 @@ void print_alphabet(void)
 		_putchar(alph);
 
 	_putchar('\n');
-	return;
 }
