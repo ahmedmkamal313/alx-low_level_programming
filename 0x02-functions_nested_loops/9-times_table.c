e244c4239cc34070b3ea67f591129b29daa23a27
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-21 15:17:12 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,13 @@
 void times_table(void)
 {
 	int i, j;
+	int k = i * j;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar("%i,", i * j);
+			_putchar(k);
 		}
 		_putchar('\n');
 	}
