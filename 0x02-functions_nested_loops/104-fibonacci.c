c917df2a29df803aad9b3278055c3647661a9a49
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-21 17:32:35 -0700

    add file and desc

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,18 @@
 
 int main(void)
 {
-	int n1 = 1;
-	int n2 = 2;
-	int n3;
-	int i;
+	int first = 1;
+	int second = 2;
+	int next, i;
 
-	printf("%d, %d", n1, n2);
+	printf("%d, %d", first, second);
 
-	for (i = 3; i <= 98; i++)
+	for (i = 3 ; i <= 98 ; i++)
 	{
-		n3 = n1 + n2;
-		printf(", %d", n3);
-		n1 = n2;
-		n2 = n3;
+		next = first + second;
+		printf(", %d", next);
+		first = second;
+		second = next;
 	}
 
 	printf("\n");
