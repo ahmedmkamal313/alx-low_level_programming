2262df1a967740f0626883e2a29a4bd8732c2063
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-23 05:32:34 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - rints the numbers, from 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n =< 9 ; n++)
+		_putchar(n);
+
+	putchar('\n');
+}
