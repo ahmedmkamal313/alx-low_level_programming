a9680630578fa6648b9b356b2e608cdb6b865128
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-23 12:55:14 -0700

    file add and update desc

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long int n;
+
+	n = 612852475143;
+
+	long int div = 2, ans = 0, maxFact;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%d", maxFact);
+				ans = 1;
+				break;
+			}
+		}
+	}
+
+	return (0);
+}
