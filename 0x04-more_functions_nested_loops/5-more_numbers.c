9cd51a03e21090372c2af46f7fd75b11ef053d9d
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-23 06:35:14 -0700

    file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,10 @@ void more_numbers(void)
 	int i;
 	int j = 0;
 
-	while (j++ < 10)
+	while (j++ <= 10)
 	{
-		for (i = 0 ; i <= 14 ; i++)
-			_putchar(i + '0');
+		for (i = 0 ; i < 15 ; i++)
+			_putchar(i);
 
 		_putchar('\n');
 	}
