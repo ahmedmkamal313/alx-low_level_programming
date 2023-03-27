13aeb42144acca6cbf549c5552780b1e4a60f67e
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-27 15:45:27 -0700

    test

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,20 +8,72 @@
  * Return: 0.
  */
 
-int main(void)
+void randomPasswordGeneration(int N)
 {
-	int i;
-	char key[10];
-
-	srand(time(NULL));
-
-	for (i = 0 ; i < 11 ; i++)
-	{
-		key[i] = (rand() % 94) + 33;
-	}
-
-	key[9] = '\0';
-	printf("%s\n", key);
-
-	return (0);
+    // Initialize counter
+    int i = 0;
+  
+    int randomizer = 0;
+  
+    // Seed the random-number generator
+    // with current time so that the
+    // numbers will be different every time
+    srand((unsigned int)(time(NULL)));
+  
+    // Array of numbers
+    char numbers[] = "0123456789";
+  
+    // Array of small alphabets
+    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+  
+    // Array of capital alphabets
+    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+  
+    // Array of all the special symbols
+    char symbols[] = "!@#$^&*?";
+  
+    // Stores the random password
+    char password[N];
+  
+    // To select the randomizer
+    // inside the loop
+    randomizer = rand() % 4;
+  
+    // Iterate over the range [0, N]
+    for (i = 0; i < N; i++) {
+  
+        if (randomizer == 1) {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2) {
+            password[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3) {
+            password[i] = LETTER[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else {
+            password[i] = letter[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+}
+  
+// Driver Code
+int main()
+{
+    // Length of the password to
+    // be generated
+    int N = 10;
+  
+    // Function Call
+    randomPasswordGeneration(N);
+  
+    return 0;
 }
