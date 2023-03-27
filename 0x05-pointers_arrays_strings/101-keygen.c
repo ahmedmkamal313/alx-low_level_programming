a66ef851d485a1a4ab26d5a7693e2774f936f038
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-27 15:27:14 -0700

    fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-/** main -  generates random valid passwords for the program 101-crackme.
+/**
+ * main -  generates random valid passwords for the program 101-crackme.
  * Return: 0.
  */
 
@@ -11,13 +12,16 @@ int main(void)
 {
 	int i;
 	char key[10];
+
 	srand(time(NULL));
 
 	for (i = 0 ; i < 10 ; i++)
+	{
 		key[i] = (rand() % 94) + 33;
+	}
 
 	key[9] = '\0';
 	printf("%s\n", key);
 
-	 return 0;
+	return (0);
 }
