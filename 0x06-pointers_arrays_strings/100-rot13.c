82f079f3bdd5d843b4c53b557889767c8a76232d
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-03-29 15:52:54 -0700

    file add and update readme

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: string to be encoded.
+ * Return: r
+ */
+
+char *rot13(char *s)
+{
+	char rot, *r = s;
+
+	while (*s)
+	{
+		rot = (*s & 32) + 65;
+		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+		{
+			*s = (*s - rot + 13) % 26 + rot;
+		}
+		s++
+	}
+
+	return (r);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -15,3 +15,5 @@
 6-cap_string.c: a function that capitalizes all words of a string.
 
 7-leet.c: a function that encodes a string into 1337.
+
+100-rot13.c: a function that encodes a string using rot13.
