2c30d548e7ea7847f1a14dbdd8e46039df0e8841
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-03 06:19:41 -0700

    2-strchr.c file add and update desc

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the string to be checked
+ * @c: the char to find in s.
+ * Return: c if found, NULL if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,3 +5,5 @@ readme
 0-memset.c:  a function that fills memory with a constant byte.
 
 1-memcpy.c: a function that copies memory area.
+
+2-strchr.c: a function that locates a character in a string.
