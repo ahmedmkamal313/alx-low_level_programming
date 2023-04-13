469e255d5a167d7278d8450024fd9c55b0f287e9
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-13 13:42:30 -0700

    fix file 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,7 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (i = 0; i < old_size && i < new_size; i++)
-		*((char *)new_ptr + i) = *((char *)ptr) + i;
+		*((char *)new_ptr + i) = *((char *)ptr + i);
 
 	free(ptr);
 
