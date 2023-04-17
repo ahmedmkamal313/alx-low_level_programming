d213a8cc7de76cd1e7e7d6466448f19aeefc9934
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-17 13:00:28 -0700

    fix requirement in file 4.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog-> name = malloc(strlen(name) + 1);
+	new_dog->name = malloc(strlen(name) + 1);
 
 	if (new_dog->name == NULL)
 	{
