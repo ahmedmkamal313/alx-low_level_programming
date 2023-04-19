73f23466da5e601936ad6a9b3e4903fcb8c7b6be
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-19 03:05:40 -0700

    create

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: the name.
+ * @f: function pointer.
+ */
+
+void print_name(char *name, void (*f)(char *)) 
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+							0x0F-function_pointers
+
+0-print_name.c: a function that prints a name.
