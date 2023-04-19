fb4d255899b9fda93231764b79a1e52cdaf9ac08
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-19 04:58:32 -0700

    file 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function.
+ * @argc: argc
+ * @argv: argv
+ * Return: 1, 2 ,0
+ */
+
+int main(int argc, char **argv)
+{
+	int num_bytes, i;
+	unsigned char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_bytes = atoi(argv[1]);
+	if (num_bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	ptr = (unsigned char *)main;
+
+	for (i = 0; i < num_bytes; i++)
+		printf("%02x", *(ptr + i));
+
+	printf("\n");
+	return (0);
+}
