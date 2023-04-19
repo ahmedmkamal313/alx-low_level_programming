055553d837f2d48be8195664e5be7012dc896a20
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-19 15:35:32 -0700

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,8 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int (*op)(int, int);
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
+	int num1, num2;
 
 	if (argc != 4)
 	{
@@ -21,19 +20,22 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	op = get_op_func(argv[2]);
 
-	if (!op)
+	if (op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if (op  == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	printf("%d\n", op(num1, num2));
 	return (0);
