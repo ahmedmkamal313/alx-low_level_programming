05331848fb47cd8785a9404f61725592c696d521
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-04-19 05:15:48 -0700

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,20 +16,22 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
 	num_bytes = atoi(argv[1]);
 	if (num_bytes <= 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
 	ptr = (unsigned char *)main;
 
 	for (i = 0; i < num_bytes; i++)
+	{
 		printf("%02x", *(ptr + i));
+	}
 
 	printf("\n");
 	return (0);
