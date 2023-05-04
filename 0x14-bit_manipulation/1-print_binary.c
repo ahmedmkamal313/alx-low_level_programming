2b629b8fbe09605f2db84dc183ad364efd8ffd0b
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-05-04 03:32:17 -0700

    fix some requirment

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,11 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return;
 	}
 
 	print_binary(n >> 1);
 
-	printf("%lu", n & 1);
+	_putchar((n & 1) + '0');
 }
