536563ceee994682d32560307b7efc30c03a20cd
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-15 10:08:56 -0700

    added 2-add_dnodeint.c, updated: list.s and readme

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to the head of the list.
+ * @n: the data of the new node.
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new; /* declare a new node pointer */
+
+	new = malloc(sizeof(dlistint_t)); /* allocate memory for the new node */
+	if (new == NULL) /* check if allocation failed */
+		return (NULL);
+	new->n = n; /* assign the data to the new node */
+	new->prev = NULL; /* set the previous pointer of the nod to null */
+	new->next = *head; /*set the next pointer of the new node to the current head */
+	if (*head != NULL) /* check if the list is not empty */
+		(*head)->prev = new; /*set the prev pointer of the current head to the new node*/
+	*head = new;
+	return (new);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -25,7 +25,10 @@
   - a function that prints all the elements of a `dlistint_t` list.
     - Prototype: `size_t print_dlistint(const dlistint_t *h);`.
     - Return: the number of nodes.
-
 **1-dlistint_len.c:**
   - a function that returns the number of elements in a linked `dlistint_t` list.
     - Prototype: `size_t dlistint_len(const dlistint_t *h);`
+**2-add_dnodeint.c:**
+  - a function that adds a new node at the beginning of a `dlistint_t list`.
+    - Prototype: `dlistint_t *add_dnodeint(dlistint_t **head, const int n);`.
+    - Return: the address of the new element, or `NULL` if it failed.
