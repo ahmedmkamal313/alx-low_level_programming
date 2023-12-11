9f0261b51afda1080d5ae47efbbabf9cec6e1b93
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-12-11 04:06:57 -0800

    updated search_algos.h with new addition

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,6 @@ void print_array(int *array, size_t left, size_t right);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
