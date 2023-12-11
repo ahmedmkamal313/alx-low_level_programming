82f45f384b4520639d1dd56e7bc0ecc33cd79eb6
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-12-11 03:38:28 -0800

    updated the header file with the new prototype functions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,12 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
+int jump_search(int *array, size_t size, int value);
 
 #endif
